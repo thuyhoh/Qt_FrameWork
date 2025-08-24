96f93602c3e1bb99b5b8d4a3799be2d9b1456dc1
Thuyh0h <trongthuynl2004@gmail.com>
2025-08-24 14:58:59 +0700

    Signals and Slots

---
+++ Qt6-Cpp/Tutorial_Templates/003_SignalsNSlots/001-Signal_Slot_UsingUIDesigner/mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->label->setText("This is changed");
+    ui->label->setFont(QFont("Times", 14));
+    ui->label->setStyleSheet("color:green");
+}
+
